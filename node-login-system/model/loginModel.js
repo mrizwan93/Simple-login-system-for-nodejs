9117f7903767fcef5966d2cc5037af90c5b931c5
rizwanshaikh1055 <mail2rizwanbsl@gmail.com>
2015-11-19 17:23:06 +0530

    --Login url

--- node-login-system/model/loginModel.js
+++ node-login-system/model/loginModel.js
@@ -1,8 +1,8 @@
 var mongoose=require('mongoose');
 var security=require('../support/security.js');
 var loginModel=new mongoose.Schema({
-	userName:{type: String, unique : true},
-	password:{type:string},
+	userName:{type: String, unique : true,sparse:true},
+	password:{type:String},
 	salt:{type:String}
 });
 
--- node-login-system/routes/routes.js
+++ node-login-system/routes/routes.js
@@ -8,12 +8,14 @@ module.exports = function(app, express) {
 	var api =  express.Router();
 
 	api.post('/register/',function(req,res){
+		console.log(req.body);
 		var registerModel = new loginCollection();
 		registerModel.userName = req.body.userName;
 		var salt=security.createSalt();
 		var pwd=security.hashPwd(salt,req.body.password);
 		registerModel.password = pwd;
 		registerModel.salt = salt;
+		console,log("saving\n"+registerModel);
 		registerModel.save(function(err,doc){
 			if(err){
 				res.send(err);
@@ -21,6 +23,25 @@ module.exports = function(app, express) {
 				res.send({
 					"msg":"User Registered!"
 				});
+			}
+		});
+	});
+
+	api.post('/login/',function(req,res){
+		loginCollection.find({"userName":req.body.userName},function(err,doc){
+			if(err){
+				res.send(err);
+			}else{
+				if(doc!=""){
+					console.log(doc);
+					if(doc.authenticate(req.body.password)){
+						res.send("Login Successfull!");
+					}else{
+						res.send("Incorrect password!")
+					}
+				}else{
+					res.send("User Does not exist!");
+				}
 			}
 		});
 	});
